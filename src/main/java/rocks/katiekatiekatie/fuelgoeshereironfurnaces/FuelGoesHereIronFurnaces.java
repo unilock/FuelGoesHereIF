0b374fb5f8210d4941117715b7acd04c54881141
kate <kate@fennet.rentals>
2023-04-03 18:06:58 -0400

    branding
    Signed-off-by: kate <kate@fennet.rentals>

---
+++ src/main/java/rocks/katiekatiekatie/fuelgoeshereironfurnaces/FuelGoesHereIronFurnaces.java
@@ -0,0 +1,9 @@
+package rocks.katiekatiekatie.fuelgoeshereironfurnaces;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("fuelgoeshereironfurnaces")
+public class FuelGoesHereIronFurnaces {
+    public FuelGoesHereIronFurnaces() {
+    }
+}
--- src/main/java/rocks/katiekatiekatie/fuelgoeshereironfurnaces/mixin/ExampleMixin.java
+++ src/main/java/rocks/katiekatiekatie/fuelgoeshereironfurnaces/mixin/ExampleMixin.java
@@ -1,6 +1,5 @@
-package com.example.examplemod.mixin;
+package rocks.katiekatiekatie.fuelgoeshereironfurnaces.mixin;
 
-import com.example.examplemod.ExampleMod;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +10,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
     }
 }
